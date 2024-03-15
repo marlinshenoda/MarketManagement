a77e76a0c51f5ade9759cb042527315d6f477d3f
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2024-03-15 12:19:29 +0100

    repositories

---
+++ MarketManagement.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using MarketManagement.Core.Entities;
+using MarketManagement.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketManagement.Data.Repositories
+{
+    public class CategoryRepository : EntityBaseRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ApplicationDbContext context) : base(context) { }
+
+    }
+}
