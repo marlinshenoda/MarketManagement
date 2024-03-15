0ac707038feee052ab89eb24f36edd935b0df61e
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2024-03-15 10:09:19 +0100

    Add project files.

---
+++ MarketManagement.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MarketManagement.Core.Entities;
+
+namespace MarketManagement.Data.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MarketManagement.Core.Entities.Category> Category { get; set; } = default!;
+        public DbSet<MarketManagement.Core.Entities.Product> Product { get; set; } = default!;
+        public DbSet<MarketManagement.Core.Entities.Transaction> Transaction { get; set; } = default!;
+    }
+}
