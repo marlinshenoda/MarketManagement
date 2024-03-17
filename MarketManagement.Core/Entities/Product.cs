7026df30b22700677a942d2b200b6741c4702476
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2024-03-17 11:05:35 +0100

    edit id

--- MarketManagement.Core/Entities/Product.cs
+++ MarketManagement.Core/Entities/Product.cs
@@ -1,10 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using eTickets.Core.Interfaces;
+using System.ComponentModel.DataAnnotations;
 
 namespace MarketManagement.Core.Entities
 {
-    public class Product
+    public class Product : IEntityBase
     {
-        public int ProductId { get; set; }
+        public int Id { get; set; }
 
         [Required]
         [Display(Name = "Category")]
--- MarketManagement.Core/Entities/Transaction.cs
+++ MarketManagement.Core/Entities/Transaction.cs
@@ -1,8 +1,10 @@
-﻿namespace MarketManagement.Core.Entities
+﻿using eTickets.Core.Interfaces;
+
+namespace MarketManagement.Core.Entities
 {
-    public class Transaction
+    public class Transaction : IEntityBase
     {
-        public int TransactionId { get; set; }
+        public int Id { get; set; }
         public DateTime TimeStamp { get; set; }
         public int ProductId { get; set; }
         public string ProductName { get; set; } = ""; // in case product name changes
