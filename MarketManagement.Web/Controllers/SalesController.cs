88698b109db92302fb32cb90d123ae799c907c9b
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2024-03-26 19:06:21 +0100

    index Sales code

--- MarketManagement.Web/Controllers/SalesController.cs
+++ MarketManagement.Web/Controllers/SalesController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarketManagement.Core.Entities.ViewModels;
+using MarketManagement.Core.Interfaces;
+using MarketManagement.Data.Data;
+using MarketManagement.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MarketManagement.Web.Controllers
 {
     public class SalesController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly IProductRepository _service;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public SalesController(ApplicationDbContext context, IProductRepository service, ICategoryRepository categoryRepository) 
         {
-            return View();
+            _context = context;
+            _service = service;
+            _categoryRepository = categoryRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var salesViewModel = new SalesViewModel
+            {
+                Categories = await _categoryRepository.GetAllAsync()
+            };
+            return View(salesViewModel);
         }
     }
 }
