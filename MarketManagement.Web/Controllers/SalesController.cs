dee5b9274ca20e24dea0b6e2fc50a9a7be8f5c44
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2024-03-26 18:54:37 +0100

    salescontroller

---
+++ MarketManagement.Web/Controllers/SalesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketManagement.Web.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
