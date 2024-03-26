bd353273d02aed33303b346f30026795321d6e58
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2024-03-26 18:52:18 +0100

    sales view model

---
+++ MarketManagement.Core/Entities/ViewModels/SalesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MarketManagement.Core.Entities.ViewModels
+{
+    public class SalesViewModel
+    {
+        public int SelectedCategoryId { get; set; }
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+
+        public int SelectedProductId { get; set; }
+
+        [Display(Name = "Quantity")]
+        [Range(1, int.MaxValue)]
+   //     [SalesViewModel_EnsureProperQuantity]
+        public int QuantityToSell { get; set; }
+    }
+}
