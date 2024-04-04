4841dba5db205b7f46ea77fea6a18b44eb7f81dd
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2024-04-04 20:45:11 +0200

    ¨move script to site.js

--- MarketManagement.Web/wwwroot/js/site.js
+++ MarketManagement.Web/wwwroot/js/site.js
@@ -7,4 +7,20 @@ $(document).ready(function () {
         $(".product-row").removeClass("highlight");
         $(this).addClass("highlight");
     });
+    $("#categoryDropdown").change(function () {
+        var SelectedCategoryId = $(this).val();
+        $.ajax({
+            url: "/Sales/GetProductsByCategoryIdAjax", // Update the controller and action names accordingly
+            type: 'GET',
+            data: { SelectedCategoryId: SelectedCategoryId },
+            success: function (response) {
+                $("#cont-category-product").html(response);
+            },
+            error: function () {
+                alert('Error occurred while fetching products.');
+            }
+        });
+
     });
+});
+
